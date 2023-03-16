93bdd7198bf85c22656e47a257109b95c203632c
dthomas94 <danielle.thomas313@gmail.com>
2023-03-16 11:56:27 -0400

    tractcard code cleanup

--- planet/src/common/Tract/TractCard.tsx
+++ planet/src/common/Tract/TractCard.tsx
@@ -13,12 +13,15 @@ export const TractCard = ({ data }: TractCardProps) => {
     <>
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <Typography variant="h5" gutterBottom>
             {data.NAMELSAD}
           </Typography>
-          <Typography variant="h5" component="div"></Typography>
-          <Typography variant="body2">LAT: {data.INTPTLAT}</Typography>
-          <Typography variant="body2">LON: {data.INTPTLON}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            LAT: {data.INTPTLAT}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            LON: {data.INTPTLON}
+          </Typography>
         </CardContent>
         <CardActions>
           <Link to={`/tracts/${data.fid}`}>View Tract Page</Link>
