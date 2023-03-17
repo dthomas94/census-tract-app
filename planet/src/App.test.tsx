e3224fc3ace735160b46049e11926cdb4b7ba428
dthomas94 <danielle.thomas313@gmail.com>
2023-03-17 13:06:02 -0400

    update test assertion

--- planet/src/App.test.tsx
+++ planet/src/App.test.tsx
@@ -4,5 +4,5 @@ import App from "./App";
 test("renders tract cards", async () => {
   render(<App />);
   const tractCards = await screen.findAllByTestId("tract-card");
-  expect(tractCards).toBeInTheDocument();
+  expect(tractCards).toHaveLength(1505);
 });
