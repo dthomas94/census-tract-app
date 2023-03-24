433b331c4ac7a3f6cfa53da41962d06888c114b3
dthomas94 <danielle.thomas313@gmail.com>
2023-03-24 17:21:19 -0400

    displays loading icon when tracts are being fetched

--- planet/src/App.tsx
+++ planet/src/App.tsx
@@ -4,14 +4,18 @@ import { getTracts } from "./api/tracts";
 import { Tract } from "./types/Tract";
 import { Pagination } from "./common/Pagination/Pagination";
 import "./App.css";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
   const [tracts, setTracts] = useState<Tract[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const asyncGetTracts = async () => {
+      setIsLoading(true);
       const tracts = await getTracts();
       setTracts(tracts);
+      setIsLoading(false);
     };
     asyncGetTracts();
   }, []);
@@ -24,7 +28,15 @@ function App() {
       justifyContent="space-between"
       columnGap={1}
     >
-      <Pagination items={tracts} />
+      {isLoading ? (
+        <CircularProgress
+          className="loader-icon"
+          style={{ height: 100, width: 100 }}
+          size="lg"
+        />
+      ) : (
+        <Pagination items={tracts} />
+      )}
     </Grid>
   );
 }
